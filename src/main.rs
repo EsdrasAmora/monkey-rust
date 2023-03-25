ea4d7510dd9b628eb52f0e2b0878955dd3b655d4
EsdrasAmora <esdras.amora@gmail.com>
2023-03-25 01:23:02 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let mut lines = io::stdin().lock().lines();
+
+    while let Some(line) = lines.next() {
+        let last_input = line.expect("Unable to read line from stdin");
+
+        if last_input.len() == 0 {
+            println!("Empty line, exiting");
+            break;
+        }
+    }
+}
