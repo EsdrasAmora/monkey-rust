fe673ae8977c26bef2a4071a9e4560529004fb25
EsdrasAmora <esdras.amora@gmail.com>
2023-03-25 18:54:20 -0300

    temp123

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,5 +1,20 @@
 mod token;
 
+#[allow(dead_code)]
+#[derive(Debug, Default)]
+pub struct Lexer {
+    input: String,
+    position: u32,      // current position in input (points to current char)
+    read_position: u32, // current reading position in input (after current char)
+    char: char,         // current char under examination
+}
+
+impl Lexer {
+    fn new(input: String) -> Self {
+        Lexer::default()
+    }
+}
+
 pub fn visibility_test() {
-    println!("Hello world, {:?}", token::Token::Value);
+    println!("Hello world, {:?}", token::Token::LET);
 }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,4 +1,21 @@
 #[derive(Debug, Clone)]
 pub enum Token {
-    Value,
+    ILLEGAL,
+    EOF,
+    // Identifiers + literals
+    IDENT,
+    INT,
+    // Operators
+    ASSIGN,
+    PLUS,
+    // Delimiters
+    COMMA,
+    SEMICOLON,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    // Keywords
+    FUNCTION,
+    LET,
 }
