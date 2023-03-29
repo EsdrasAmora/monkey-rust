427ee042e7d3fa0e327a80168c50b5ab9952b403
EsdrasAmora <esdras.amora@gmail.com>
2023-03-29 00:29:52 -0300

    gg321

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,42 +2,48 @@ mod ast;
 
 use std::iter::Peekable;
 
+use anyhow::{bail, Error, Result};
+
 //TODO: how to rexport this?
 use crate::lexer::token::Token;
 use crate::lexer::Lexer;
 
 use self::ast::{Literal, Node};
 
-struct Program {
+struct Parser {
     nodes: Vec<Node>,
+    errors: Vec<Error>,
 }
 
-impl Program {
+impl Parser {
     fn new(lexer: Lexer) -> Self {
         let mut nodes = Vec::with_capacity(32);
+        let mut errors = Vec::with_capacity(8);
         let mut tokens = lexer.tokens.into_iter().peekable();
 
         while let Some(current) = tokens.next() {
-            //TODO: is this more readable than `if let some`?
-            Self::new_helper(&current, &mut tokens).map(|x| nodes.push(x));
+            Self::new_helper(&current, &mut tokens)
+                .map_or_else(|err| errors.push(err), |val| nodes.push(val))
         }
 
-        Program { nodes }
+        Parser { nodes, errors }
     }
 
-    //TODO:return an result
     fn new_helper(
         current: &Token,
         tokens: &mut Peekable<impl Iterator<Item = Token>>,
-    ) -> Option<Node> {
+    ) -> Result<Node> {
         match current {
             Token::Let => {
                 //currently does not autoformat lmao: https://github.com/rust-lang/rustfmt/issues/4914
-                let Some(Token::Identifier(_)) = tokens.peek() else { return None; };
-                let Some(Token::Identifier(name)) = tokens.next() else{ return None; };
+                let Some(Token::Identifier(_)) = tokens.peek() else { bail!("expected token to be {:?}, got {:?} instead",Token::Identifier("foo".to_owned()), tokens.peek()) };
+                let Some(Token::Identifier(name)) = tokens.next() else{ unreachable!() };
 
                 if tokens.peek() != Some(&Token::Assign) {
-                    return None;
+                    bail!(
+                        "expected assign after indentifier found: {:?}",
+                        tokens.peek()
+                    )
                 };
                 tokens.next();
 
@@ -48,9 +54,9 @@ impl Program {
                     value: Box::new(Node::Literal(Literal::Int(5))),
                 };
 
-                Some(let_statment)
+                Ok(let_statment)
             }
-            _ => None,
+            _ => bail!("Cannot parse an statment starting with {:?}", current),
         }
     }
 }
@@ -63,17 +69,17 @@ mod tests {
     #[test]
     fn parse_let_statement() {
         let input = "
-        let x = 5;
+        let x = false;
         let y = 10;
-        let foobar = 838383;";
+        let foobar = true;";
 
         let lexer = Lexer::new(input);
-        let program = Program::new(lexer);
+        let program = Parser::new(lexer);
 
-        let result = program.nodes;
+        assert!(program.errors.is_empty(), "errors: {:#?}", program.errors);
 
         assert_eq!(
-            result,
+            program.nodes,
             [
                 Node::Let {
                     indentifier: "x".to_string(),
